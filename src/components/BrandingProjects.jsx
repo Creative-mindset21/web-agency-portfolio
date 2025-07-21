49083c68d5812fa00a82b5c633f58acab6fc2412
Creative Mindset <tobiawotoye03@gmail.com>
2025-07-21 05:42:19 +0100

    updated the layout of my branding projects

--- src/components/BrandingProjects.jsx
+++ src/components/BrandingProjects.jsx
@@ -7,9 +7,11 @@ const BrandingProjects = () => {
         <div className='wrapper'>
             <ul className="branding-cards">
                 {brandingData.map((item) => (
-                    <li className="card" key={item.id}>
+                    <li className="card">
                         <img src={item.imgSrc} alt={`Image of ${item.name}`} />
-                            <p className='mt-2 font-bold text-nav-color'>{item.name.toUpperCase()}</p>
+                        <p className='mt-2 font-bold text-nav-color'>
+                            {item.name.toUpperCase()}
+                        </p>
                     </li>
                 ))} 
             </ul>
