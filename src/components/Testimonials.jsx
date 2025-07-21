08d9b941a5fb19ca399f6b17a823419f913cca18
Creative Mindset <tobiawotoye03@gmail.com>
2025-07-21 15:02:35 +0100

    styled the testimonial section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,12 +4,21 @@ import { testimonialData } from '../data/main'
 const Testimonials = () => {
   return (
     <section>
-        <div className="wrapper lg:text-center py-20">
+        <div className="wrapper lg:text-center lg:py-15 py-10">
             <h2 className='text-5xl mb-8'>Real Clients Testimonials</h2>
             <h3>WRITE US A REVIEW</h3>
 
             <ul className="testimonial-cards">
-                
+                {testimonialData.map((item) => (
+                    <li className='testimonial-card' key={item.id}>
+                        <p>{item.message}</p>
+
+                        <div className="flex flex-col gap-1 mt-2">
+                            <h2 className='font-bold text-lg'>{item.name}</h2>
+                            <h3>{item.occupation}</h3>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     </section>
