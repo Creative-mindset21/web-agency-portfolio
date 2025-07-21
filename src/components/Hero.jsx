f02a61dde59712517b15b8f4c90d55cdcb8bf98b
Creative Mindset <tobiawotoye03@gmail.com>
2025-07-21 14:29:02 +0100

    updated hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,11 +11,11 @@ const Hero = () => {
         <section className=" flex flex-col pt-20 lg:pt-30 lg:px-10 md:h-[70%] lg:h-screen text-center gap-5 justify-center items-center wrapper">
             <div className="flex flex-col lg:gap-6 w-full relative">
               <div className="flex flex-col md:gap-3 text-left md:pt-10">
-                <span className="text-5xl md:text-[7rem] lg:text-[9.5rem] font-bricolage leading-10 mt-3 font-bold lg:text-left lg:leading-24">Good Designs</span><br /> 
+                <span className="text-[3.4rem] md:text-[7rem] lg:text-[9.5rem] font-bricolage leading-10 mt-3 font-bold lg:text-left lg:leading-24">Good Designs</span><br /> 
 
-                <span className="text-5xl md:text-[7rem] md:mt-6 lg:text-[9.5rem] font-bricolage leading-10 font-bold lg:text-right lg:leading-24">Make Great</span><br />
+                <span className="text-[3.4rem] md:text-[7rem] md:mt-6 lg:text-[9.5rem] font-bricolage leading-10 font-bold lg:text-right lg:leading-24">Make Great</span><br />
 
-                <span className="text-5xl md:text-[7rem] md:mt-6 lg:text-[9.5rem] font-bricolage leading-10  font-bold lg:text-left lg:leading-24">Business</span>
+                <span className="text-[3.4rem] md:text-[7rem] md:mt-6 lg:text-[9.5rem] font-bricolage leading-10  font-bold lg:text-left lg:leading-24">Business</span>
                
               </div>
               <div className="flex flex-col md:flex-row text-left mt-5 md:mt-15 lg:absolute bottom-0 right-0">
